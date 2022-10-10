adfc7ef16041d25bcd01846745d161846ffbbdb3
mohtadahmed <mohtadsaeem13@gmail.com>
2022-10-10 15:20:26 +0600

    three input parameter in arrow function to multiply them

--- arrowFunction.js
+++ arrowFunction.js
@@ -9,3 +9,8 @@ console.log(dividedByFive(50));
 // Declaring an Arrow Function which will take two input parameter and add 2 to both of the parameter and multiply them.
 const sum = (first, second) => (first + 2) * (second + 2);
 console.log(sum(10, 20));
+
+
+// Declaring an Arrow Function which will take three input parameter and multiply them.
+const multiply = (firstNumber, secondNumber, thirdNumber) => firstNumber * secondNumber * thirdNumber;
+console.log(multiply(3, 5, 7));
