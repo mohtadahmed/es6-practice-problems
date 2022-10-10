2cddede6a8005da0a1d5f46ecc17105f89778355
mohtadahmed <mohtadsaeem13@gmail.com>
2022-10-10 14:46:11 +0600

    declaring arrow function with single parameter

---
+++ arrowFunction.js
@@ -0,0 +1,6 @@
+// Declaring an Arrow Function with a single parameter.
+// This Arrow Function will take a single input and divide the value by 5 and give the output.
+
+const dividedByFive = number => number / 5;
+
+console.log(dividedByFive(50));
