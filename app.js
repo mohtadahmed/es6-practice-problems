b28f7e53558471a13f1abdf81faac4c7b214003a
mohtadahmed <mohtadsaeem13@gmail.com>
2022-10-10 14:41:55 +0600

    template string for dynamic use of variable

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 // Declaring a variable using Let
 let age = 24;
+let profession = 'student';
 
 // Declaring a variable using Const
-const name = 'Anim Hossain'
+const fullName = 'Anim Hossain';
+const departmentName = 'EEE';
+const address = 'Chittagong';
+
+
+// Declaring a String using Template String
+
+const info = `Hi! My name is ${fullName}, I am ${age} years old. I am a ${profession} from deparment of ${departmentName}. I live in ${address}`;
+// console.log(info);
